032159e124012007b071096ac3f191fd163bdeee
Pablo Mendoza <pawnil005@gmail.com>
2024-05-18 23:25:50 -0500

    move enemy correction

--- Assets/Scrips/MoveScrips/AtackRangeEnemy.cs
+++ Assets/Scrips/MoveScrips/AtackRangeEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AtackRangeEnemy : MoveEnemy
+public class AtackRangeEnemy : MoveController
 {
     [SerializeField] protected GameObject prefab;
     [SerializeField] protected Transform shootPoint;
@@ -30,5 +30,15 @@ public class AtackRangeEnemy : MoveEnemy
         GameObject obj = Instantiate(prefab);
         obj.transform.position = shootPoint.position;
         obj.GetComponent<Bullet>().SetDirection(shootPoint.forward);
+    }
+    protected override void Move()
+    {
+        if (Player != null && Vector3.Distance(transform.position, Player.position) >= distanceAtack)
+        {
+            Vector3 direction = Player.position - transform.position;
+            direction.Normalize();
+            transform.position += direction * maxSpeed * Time.deltaTime;
+            transform.LookAt(Player.position);
+        }
     }
 }
--- Assets/Scrips/MoveScrips/MoveEnemy.cs
+++ Assets/Scrips/MoveScrips/MoveEnemy.cs
@@ -6,12 +6,12 @@ public class MoveEnemy : MoveController
 {
     protected override void Move()
     {
-        if (Player != null && Vector3.Distance(transform.position, Player.position) >= distanceAtack)
+        if (Player != null && Vector3.Distance(transform.position, Player.position) >= 0)
         {
             Vector3 direction = Player.position - transform.position;
             direction.Normalize();
             transform.position += direction * maxSpeed * Time.deltaTime;
             transform.LookAt(Player.position);
         }
-    }
+    } 
 }
